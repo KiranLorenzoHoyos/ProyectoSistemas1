4f653cf0b07e22c5b93f8f2faaf94992ac34d76a
KiranLorenzoHoyos <kiran.lorenzo@gmail.com>
2018-03-22 04:53:19 -0700

    Cambio de Hola a Hello!

--- MiPrimerProyecto/ViewController.swift
+++ MiPrimerProyecto/ViewController.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var lblPrueba:UILabel?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        lblPrueba?.text = "HELLO!"
     }
 
     override func didReceiveMemoryWarning() {
