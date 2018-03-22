3319075e18ee1df92a46e45d5a0706dc6584c623
KiranLorenzoHoyos <kiran.lorenzo@gmail.com>
2018-03-22 03:06:40 -0700

    Initial Commit

---
+++ MiPrimerProyecto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiPrimerProyecto
+//
+//  Created by KIRAN LORENZO HOYOS on 22/3/18.
+//  Copyright © 2018 KIRAN LORENZO HOYOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
