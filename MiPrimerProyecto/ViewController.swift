ade8d3671ac7365974ab8480b602da31685efa02
KiranLorenzoHoyos <kiran.lorenzo@gmail.com>
2018-04-03 14:58:19 +0200

    clase 3 abril

--- MiPrimerProyecto/ViewController.swift
+++ MiPrimerProyecto/ViewController.swift
@@ -4,12 +4,14 @@
 //
 //  Created by KIRAN LORENZO HOYOS on 22/3/18.
 //  Copyright © 2018 KIRAN LORENZO HOYOS. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var lblPrueba:UILabel?
+    @IBOutlet var txtUser:UITextField?
+    @IBOutlet var txtUPass:UITextField?
+    @IBOutlet var btnLogin:UITextField?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,7 +22,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func eventoClickLogin(){
+        print("HEY!"+(txtUser?.text)!)
+        if txtUser?.text == "Yony"{
+            self.performSegue(withIdentifier: "trlogin", sender: self)
+        }
+    }
 
 }
 
---
+++ VCRegistreViewController.swift
@@ -0,0 +1,35 @@
+//
+//  VCRegistreViewController.swift
+//  MiPrimerProyecto
+//
+//  Created by KIRAN LORENZO HOYOS on 3/4/18.
+//  Copyright © 2018 KIRAN LORENZO HOYOS. All rights reserved.
+//
+
+import UIKit
+
+class VCRegistreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
